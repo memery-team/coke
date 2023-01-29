759f7094f6493e7bbb187cc4f94d143c71868422
Gia Bao <giabao032002@gmail.com>
2023-01-29 17:39:28 +0700

    fix login ui

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -27,61 +27,65 @@ const Login = () => {
     }
 
     return (
-        <Form
-            name="normal_login"
-            className="login-form"
-            initialValues= {{
-                remember: true,
-            }}
-            onFinish={onFinish}
-        >
-            <Form.Item
-                name="email"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your Email!',
-                    },
-                ]}
+        <div className="login-container">
+            <Form
+                name="normal_login"
+                className="login-form"
+                initialValues= {{
+                    remember: true,
+                }}
+                onFinish={onFinish}
             >
-                <Input prefix={<UserOutlined className="site-form-item-icon" />}
-                type="email"
-                placeholder="Email" />
-            </Form.Item>
-
-            <Form.Item
-                name="password"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your Password!',
-                    },
-                ]}
-            >
-                <Input
-                    prefix={<LockOutlined className="site-form-item-icon" />}
-                    type="password"
-                    placeholder="Password"
-                />
-            </Form.Item>
-
-            <Form.Item>
-                <Form.Item name="remember" valuePropName="checked" noStyle>
-                    <Checkbox>Remember me</Checkbox>
+                <h1>Memery</h1>
+                <h2>Log In</h2>
+                <Form.Item
+                    name="email"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Email!',
+                        },
+                    ]}
+                >
+                    <Input prefix={<UserOutlined className="site-form-item-icon" />}
+                    type="email"
+                    placeholder="Email" />
                 </Form.Item>
 
-                <a className="login-form-forgot" href="">
-                    Forgot password
-                </a>
-            </Form.Item>
+                <Form.Item
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Password!',
+                        },
+                    ]}
+                >
+                    <Input
+                        prefix={<LockOutlined className="site-form-item-icon" />}
+                        type="password"
+                        placeholder="Password"
+                    />
+                </Form.Item>
 
-            <Form.Item>
-                <Button type="primary" htmlType="submit" className="login-form-button">
-                    Log in
-                </Button>
-                Or <a href="">register now!</a>
-            </Form.Item>
-        </Form>
+                <Form.Item>
+                    <Form.Item name="remember" valuePropName="checked" noStyle>
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+
+                    <a className="login-form-forgot" href="">
+                        Forgot password
+                    </a>
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" className="login-form-button">
+                        Log in
+                    </Button>
+                    Or <a href="">register now!</a>
+                </Form.Item>
+            </Form>
+        </div>
     );
 };
 
