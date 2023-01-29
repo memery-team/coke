6dd4952766341d9212ae976fb58d3c1e2a9f52b9
Gia Bao <giabao032002@gmail.com>
2023-01-29 13:30:26 +0700

    fix error, warning & ui

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,23 +1,44 @@
-import React, { useState } from "react"
-import axios from "axios"
-import "./Home.css"
-import { Table, Layout } from 'antd'
+import React, { useState } from "react";
+import axios from "axios";
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme, Table } from 'antd';
 
-const { Header, Content, Footer, Sider } = Layout
+const { Header, Content, Footer, Sider } = Layout;
 const { Column } = Table
 const LIST_CATEGORIES_URL = `http://localhost:4000/v1/categories`
 
-const data2 = [
+const navbar = ['1', '2', '3'].map((key) => ({
+    key,
+    label: `nav ${key}`,
+}));
 
-]
+const sidebar = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
+    const key = String(index + 1);
+    return {
+        key: `sub${key}`,
+        icon: React.createElement(icon),
+        label: `subnav ${key}`,
+        children: new Array(4).fill(null).map((_, j) => {
+        const subKey = index * 4 + j + 1;
+        return {
+            key: subKey,
+            label: `option${subKey}`,
+        };
+        }),
+    };
+});
 
 const Home = () => {
-    const [rawData, setRawData] = useState(null)
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
+
+    const [data, setData] = useState(null)
 
     const getData = async () => {
         try {
             await axios.get(LIST_CATEGORIES_URL).then((response) => {
-                setRawData(response.data.docs)
+                setData(response.data.docs)
             })
         } catch (err) {
             console.log(err)
@@ -26,29 +47,74 @@ const Home = () => {
     }
 
     getData()
+
     return (
-        <div className="home">
-            <Layout>
-                <Header className="header">Memery</Header>
-                <Layout>
-                    <Sider>left sidebar</Sider>
-                    <Content>
-                        <Layout>
-                            <Table dataSource={rawData} className="table">
-                                <Column title="Name" dataIndex="name" key="name" />
-                                <Column title="Description" dataIndex="description" key="description" />
-                                <Column title="Created At" dataIndex="createdAt" key="createdAt" />
-                                <Column title="Update At" dataIndex="updatedAt" key="updatedAt" />
-                                <Column title="Enabled" dataIndex="enabled" key="enabled" />
-                            </Table>
-                        </Layout>
+        <Layout>
+            <Header className="header">
+                <div className="logo" />
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} items={navbar} />
+            </Header>
+            <Content
+                style={{
+                    padding: '0 50px',
+                }}
+            >
+                <Breadcrumb
+                    style={{
+                        margin: '16px 0',
+                    }}
+                >
+                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item>List</Breadcrumb.Item>
+                    <Breadcrumb.Item>User</Breadcrumb.Item>
+                </Breadcrumb>
+                <Layout
+                    style={{
+                        padding: '24px 0',
+                        background: colorBgContainer,
+                    }}
+                >
+                    <Sider
+                        style={{
+                            background: colorBgContainer,
+                        }}
+                        width={200}
+                    >
+                        <Menu
+                            mode="inline"
+                            defaultSelectedKeys={['1']}
+                            defaultOpenKeys={['sub1']}
+                            style={{
+                                height: '100%',
+                            }}
+                            items={sidebar}
+                        />
+                    </Sider>
+                    <Content
+                        style={{
+                            padding: '0 24px',
+                            minHeight: 280,
+                        }}
+                    >
+                        <Table dataSource={data} className="table" rowKey={(data) => data.name}>
+                            <Column title="Name" dataIndex="name" key="name" />
+                            <Column title="Description" dataIndex="description" key="description" />
+                            <Column title="Created At" dataIndex="createdAt" key="createdAt" />
+                            <Column title="Updated At" dataIndex="updatedAt" key="updatedAt" />
+                            <Column title="Enabled" dataIndex="enabled" key="enabled" />
+                        </Table>
                     </Content>
                 </Layout>
-                <Footer>footer</Footer>
-            </Layout>
-        </div>
-    )
-}
+            </Content>
+            <Footer
+                style={{
+                    textAlign: 'center',
+                }}
+            >
+                Memery ©2023 Footer
+            </Footer>
+        </Layout>
+    );
+};
 
-export default Home
-
+export default Home;
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,36 +1,69 @@
-import React, { useState, useRef, useEffect } from "react"
-import { Button, Form, Input } from "antd"
+
+
+
+
+// const Login = () => {
+
+    
+
+
+
+
+//     return (
+//         <div className="login-page">
+//             <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+//             <Form className="login-form">
+//                 <h2>Sign In</h2>
+//                 <label htmlFor="email"> Email </label>
+//                 <Input 
+//                     value={email} 
+//                     type="email" 
+//                     className="emailInput" 
+//                     id="email"
+//                     ref={userRef}
+//                     onChange={(e) => setEmail(e.target.value)}
+//                 />
+//                 <label htmlFor="password"> Password </label>
+//                 <Input 
+//                     value={password} 
+//                     type="password" 
+//                     className="passwordInput" 
+//                     id="password"
+//                     onChange={(e) => setPassword(e.target.value)}
+//                 />
+//                 <Button 
+//                     type="primary"
+//                     className="login-button"
+//                     onClick={handleSubmit}
+//                 > 
+//                     Sign In 
+//                 </Button>
+//             </Form>      
+//         </div>
+//     )
+// }
+
+// export default Login
+
+import React from "react"
 import { useNavigate } from "react-router-dom"
-import "./Login.css"
 import axios from "axios"   
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Form, Input } from 'antd';
+import "./Login.css"
 
 const LOGIN_URL = `http://localhost:4000/v1/auth`
 
 const Login = () => {
     const navigate = useNavigate()
-    const userRef = useRef()
-    const errRef = useRef()
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [errMsg, setErrMsg] = useState('')
+    const onFinish = async (values) => {
+        console.log('Received values of form: ', values);
 
-    useEffect(() => {
-        userRef.current.focus()
-    }, [])
-
-    useEffect(() => {
-        setErrMsg('')
-    }, [email, password])
-    
-
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
         try {
             const admin = {
-                email: email,
-                password: password,
+                email: values.email,
+                password: values.password,
             }
             const response = await axios.post(LOGIN_URL, admin) 
             console.log(response)
@@ -41,37 +74,64 @@ const Login = () => {
     }
 
     return (
-        <div className="login-page">
-            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-            <Form className="login-form">
-                <h2>Sign In</h2>
-                <label htmlFor="email"> Email </label>
-                <Input 
-                    value={email} 
-                    type="email" 
-                    className="emailInput" 
-                    id="email"
-                    ref={userRef}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <label htmlFor="password"> Password </label>
-                <Input 
-                    value={password} 
-                    type="password" 
-                    className="passwordInput" 
-                    id="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <Button 
-                    type="primary"
-                    className="login-button"
-                    onClick={handleSubmit}
-                > 
-                    Sign In 
-                </Button>
-            </Form>      
-        </div>
-    )
-}
+        <Form
+            name="normal_login"
+            className="login-form"
+            initialValues= {{
+                remember: true,
+            }}
+            onFinish={onFinish}
+        >
+            <Form.Item
+                name="email"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Email!',
+                    },
+                ]}
+            >
+                <Input prefix={<UserOutlined className="site-form-item-icon" />}
+                type="email"
+                placeholder="Email" />
+            </Form.Item>
 
-export default Login
+            <Form.Item
+                name="password"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Password!',
+                    },
+                ]}
+            >
+                <Input
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Password"
+                />
+            </Form.Item>
+
+            <Form.Item>
+                <Form.Item name="remember" valuePropName="checked" noStyle>
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <a className="login-form-forgot" href="">
+                    Forgot password
+                </a>
+            </Form.Item>
+
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                    Log in
+                </Button>
+                Or <a href="">register now!</a>
+            </Form.Item>
+        </Form>
+    );
+};
+
+export default Login;
+
+
