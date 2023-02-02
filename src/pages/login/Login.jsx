de5035f2a22ec42437ed9bb76b226d4c2e752928
Gia Bao <giabao032002@gmail.com>
2023-02-02 20:12:26 +0700

    fix warning & add loading button

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,14 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import axios from "axios"   
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Checkbox, Form, Input, Spin, ConfigProvider } from 'antd';
 import "./Login.css"
 
 const LOGIN_URL = `https://latte-staging-wceiv3ifnq-an.a.run.app/v1/auth`
 
 const Login = () => {
     const navigate = useNavigate()
+
+    const [loading, setLoading] = useState(false)
 
     const onFinish = async (values) => {
         console.log('Received values of form: ', values);
@@ -27,65 +29,81 @@ const Login = () => {
     }
 
     return (
-        <div className="login-container">
-            <Form
-                name="normal_login"
-                className="login-form"
-                initialValues= {{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-            >
-                <h1>Memery</h1>
-                <h2>Log In</h2>
-                <Form.Item
-                    name="email"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your Email!',
-                        },
-                    ]}
+        <ConfigProvider
+            theme={{
+                components: {
+                    Spin: {
+                        colorPrimary: '#e6f4ff',
+                    },
+                },
+    }}
+        >
+            <div className="login-container">
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues= {{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
                 >
-                    <Input prefix={<UserOutlined className="site-form-item-icon" />}
-                    type="email"
-                    placeholder="Email" />
-                </Form.Item>
-
-                <Form.Item
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your Password!',
-                        },
-                    ]}
-                >
-                    <Input
-                        prefix={<LockOutlined className="site-form-item-icon" />}
-                        type="password"
-                        placeholder="Password"
-                    />
-                </Form.Item>
-
-                <Form.Item>
-                    <Form.Item name="remember" valuePropName="checked" noStyle>
-                        <Checkbox>Remember me</Checkbox>
+                    <h1>Memery</h1>
+                    <h2>Log In</h2>
+                    <Form.Item
+                        name="email"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Email!',
+                            },
+                        ]}
+                    >
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />}
+                        type="email"
+                        placeholder="Email" />
                     </Form.Item>
 
-                    <a className="login-form-forgot" href="">
-                        Forgot password
-                    </a>
-                </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Password!',
+                            },
+                        ]}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Item>
 
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" className="login-form-button">
-                        Log in
-                    </Button>
-                    Or <a href="">register now!</a>
-                </Form.Item>
-            </Form>
-        </div>
+                    <Form.Item>
+                        <Form.Item name="remember" valuePropName="checked" noStyle>
+                            <Checkbox>Remember me</Checkbox>
+                        </Form.Item>
+
+                        <a className="login-form-forgot" href="/#">
+                            Forgot password
+                        </a>
+                    </Form.Item>
+
+                    <Form.Item>
+                        <Button 
+                            type="primary" 
+                            htmlType="submit" 
+                            className="login-form-button"
+                            onClick={() => setLoading(preValue => !preValue)}
+                        >
+                            Log in
+                            <Spin spinning={loading} className="spinner" size="small" />
+                        </Button>
+                        Or <a href="/#">register now!</a>
+                    </Form.Item>
+                </Form>
+            </div>
+        </ConfigProvider>
     );
 };
 
